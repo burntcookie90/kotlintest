474328d5efab5366d95f2d9e0c7f20b4979f9eec
Vishnu Rajeevan <vrajeevan@roundarch.com>
2015-03-23 21:42:34 -0500

    update with `isEven` function

--- app/src/main/kotlin/io/dwak/kotlintest/MainActivity.kt
+++ app/src/main/kotlin/io/dwak/kotlintest/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.ActionBarActivity
 import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
 public class MainActivity : ActionBarActivity() {
     private var valueArray = Array(50000, { i -> i })
@@ -13,14 +13,21 @@ public class MainActivity : ActionBarActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val testText = findViewById(R.id.testText) as TextView
         val testButton = findViewById(R.id.testButton) as Button
         var counter = 0
         testButton.setOnClickListener {
-            valueArray.filter { it % 2 == 0 }
+            valueArray.filter { isEven(value = it) }
                     .map({ "Value: $it" })
-                    .forEach { counter++ }
-            Log.d("Test", "$counter")
+                    .forEach({
+                        Log.d("TEST", "$it")
+                        counter++
+                    })
+            testText.setText("$counter")
         }
+    }
 
+    fun isEven(value : Int) : Boolean{
+        return value % 2 == 0;
     }
 }
