2beb73e8611b62eb8c13eebe70dfd8b3b5544968
Vishnu Rajeevan <vrajeevan@roundarch.com>
2015-03-25 15:12:07 -0500

    some more kotlin work

--- app/src/main/kotlin/io/dwak/kotlintest/MainActivity.kt
+++ app/src/main/kotlin/io/dwak/kotlintest/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 
 public class MainActivity : ActionBarActivity() {
+    val TAG = this.javaClass.getSimpleName();
     private var valueArray = Array(50000, { i -> i })
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +21,39 @@ public class MainActivity : ActionBarActivity() {
             valueArray.filter { isEven(value = it) }
                     .map({ "Value: $it" })
                     .forEach({
-                        Log.d("TEST", "$it")
+                        Log.d(TAG, "$it")
                         counter++
                     })
             testText.setText("$counter")
         }
+
+        val testObject = TestObject(name = "Vishnu", id = 1)
+        Log.d(TAG, "$testObject")
+        Log.d(TAG, testObject.name)
+        Log.d(TAG, testObject.name.toAlternateCase())
     }
 
-    fun isEven(value : Int) : Boolean{
-        return value % 2 == 0;
+    fun isEven(value : Int) : Boolean = value % 2 == 0
+    fun isOdd(value : Int) : Boolean = !isEven(value)
+
+    // Play with extensions
+    fun String.toAlternateCase() : String {
+        var upperCase = false
+        var newString: String = ""
+        this.forEach {
+            when {
+                !upperCase -> {
+                    newString += Character.toUpperCase(it)
+                }
+                else -> {
+                    newString += Character.toLowerCase(it)
+                }
+            }
+
+            upperCase = !upperCase
+        }
+
+        return newString
     }
+
 }
---
+++ app/src/main/kotlin/io/dwak/kotlintest/TestObject.kt
@@ -0,0 +1,8 @@
+package io.dwak.kotlintest
+
+/**
+ * Created by vrajeevan on 3/24/15.
+ */
+
+data class TestObject(val name : String,
+                      val id : Int )
